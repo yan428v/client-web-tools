2f2904ffc230576678f9a1cac6767660e61a2492
yan <yan428@mail.ru>
2024-05-09 22:22:56 +0300

    Integrate React into the project

---
+++ src/component/Counter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Counter.scss"
+const Counter = () => {
+    return (
+        <div>
+            счетчик!
+            <button>fsdfdasfs</button>
+        </div>
+    );
+};
+
+export default Counter;
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import {testFn} from "./test";
-
-testFn(3213)
-
-// document.body.innerHTML = `<div>test text</div>`
