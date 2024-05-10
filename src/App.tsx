fb04ff2ec316baaed8742b04c951b24b2fe5ffb4
yan <yan428@mail.ru>
2024-05-10 20:18:15 +0300

    Add ReactRoutes, integrate async components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, {Suspense} from 'react';
 import "./index.scss"
+import {Routes, Route, Link} from "react-router-dom";
+import {MainPageAsync} from "./pages/MainPage/MainPageAsync";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
+
 const App = () => {
     return (
         <div className={"app"}>
-            fdsafsadfasdfsadfdfsafasdfasfs
-            <Counter/>
+            <Link to={""}>Главная</Link>
+            <Link to={"/about"}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={"/about"} element={<AboutPageAsync/>}/>
+                    <Route path={"/"} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
---
+++ src/pages/MainPage/MainPageAsync.tsx
@@ -0,0 +1,5 @@
+import {lazy} from "react";
+export const MainPageAsync = lazy(() => new Promise(resolve => {
+// @ts-ignore
+    setTimeout(() => resolve(import("./MainPage")), 1500)
+}))
