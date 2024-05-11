92763837d6c5d2282db2be8c40a496459417dc25
yan <yan428@mail.ru>
2024-05-11 15:08:29 +0300

    Add theme management using Context and custom hooks for light and dark modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, {Suspense} from 'react';
-import "./index.scss"
+import {Suspense, useContext, useState} from 'react';
+import "./styles/index.scss"
 import {Routes, Route, Link} from "react-router-dom";
 import {MainPageAsync} from "./pages/MainPage/MainPageAsync";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className={"app"}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={""}>Главная</Link>
             <Link to={"/about"}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
---
+++ src/theme/useTheme.ts
@@ -0,0 +1,21 @@
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
+import {useContext} from "react";
+
+interface UseTheme {
+    toggleTheme: () => void;
+    theme: Theme
+}
+
+export function useTheme():UseTheme {
+    const {theme, setTheme} = useContext(ThemeContext);
+
+    const toggleTheme = () => {
+        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        setTheme(newTheme);
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+    }
+    return {
+        theme,
+        toggleTheme
+    }
+}
