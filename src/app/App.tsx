a5601b03e84dee76b1ed1b4dedfd54be02866678
yan <yan428@mail.ru>
2024-05-19 00:24:10 +0300

    add i18n Language library for internationalization

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,21 +8,6 @@ import { Suspense } from "react";
 import { useTranslation } from "react-i18next";
 
 
-const Component = () => {
-    const { t, i18n } = useTranslation();
-
-     const toggle = () => {
-         i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
-     }
-    return (
-        <div>
-            <button onClick={toggle}>
-                {t("Перевод")}
-            </button>
-            {t("Тестовый пример")}
-        </div>
-    )
-}
 
 const App = () => {
     const {theme} = useTheme();
@@ -31,7 +16,6 @@ const App = () => {
         <div className={classNames("app", {}, [theme])}>
             <Suspense fallback={""}>
                 <Navbar/>
-                <Component/>
                 <div className={"content-page"}>
                     <Sidebar/>
                     <AppRouter/>
--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,8 +1,11 @@
+import {useTranslation} from "react-i18next";
 
 const AboutPage = () => {
+    const {t} = useTranslation("about");
+
     return (
         <div>
-            about page
+            {t("О сайте")}
         </div>
     );
 };
